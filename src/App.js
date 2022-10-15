e15aa3b53a70c8e810ed0ead7d105328f1ca0166
Ramazan Doğan <doganrmzn40@gmail.com>
2022-10-15 13:41:10 +0300

    Redux to Components
    Creating all components then connecting redux to components
    ..
    and finish the basic counter project with react redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Counter from "./components/Counter";
+import IncreaseCounter from "./components/IncreaseCounter";
+import DecreaseCounter from "./components/DecreaseCounter";
+import IncreaseByTwoCounter from "./components/IncreaseByTwoCounter";
 
 function App() {
   return (
     <div>
-      <h1>Redux 💻 </h1>
+      <h1> Redux 💻 </h1>
+      <Counter />
+      <IncreaseCounter />
+      <DecreaseCounter />
+      <IncreaseByTwoCounter />
     </div>
   );
 }
